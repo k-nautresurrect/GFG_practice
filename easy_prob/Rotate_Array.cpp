d441046502279d7f526fb84990d3588db164db07
k-nautresurrect <kushignite@gmail.com>
2021-10-18 10:52:18 +0530

    Rotate array

--- easy_prob/Rotate_Array.cpp
+++ easy_prob/Rotate_Array.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-int main(){
+int main() {
 	int T;  cin >> T;
-	while(T-- > 0){
+	while (T-- > 0) {
 		int n, d;  cin >> n >> d;
 		int arr[n], arrd[n], temp;
-		for(int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			cin >> arr[i];
 		}
-		for(int i = 0; i< d; i++){
-			arrd[(n-1)-i] = arr[i];
-		}
-		for(int i = 0; i < n-d; i++){
-			arrd[i] = arr[(n-d-1) + i];
+
+		for (int i = d - 1; i >= 0 ; i--) {
+			temp = arr[i];
+			arr[i] = arr[(n - 1) - i];
+			arr[(n - 1) - i] = temp;
 		}
 
 		for (int i = 0; i < n; ++i)
 		{
-			cout << arrd[i] << " ";
+			cout << arr[i] << " ";
 		}
 		cout << "\n";
 	}
