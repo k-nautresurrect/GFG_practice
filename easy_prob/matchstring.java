9775703dc858cde93776adc724d931085a90e092
k-nautresurrect <kushignite@gmail.com>
2021-10-18 10:51:11 +0530

    Matching String question gfg

--- easy_prob/matchstring.java
+++ easy_prob/matchstring.java
@@ -20,11 +20,19 @@ public class matchstring {
 		if (cntry == cntrx) {
 			for (i = 0; i < s.length(); i++) {
 				if (s.charAt(i) == 'x') { cntr++; }
-				if (s.charAt(i) == 'y') { cntr--; }
-				if (cntr == 0 && i < s.length()) { cntr = 1; }
+				if (s.charAt(i) == 'y') { break; }
 			}
-			if (cntr <= 0 || cntr > 1) { output.println(0); }
-			if (cntr == 1) { output.println(1); }
+			int cn = cntr;
+			int res = cntr;
+			for (int t = 1; t <= cntr; t++) {
+				if (s.charAt(res) == 'y') { cn--; res++; }
+				else { break; }
+			}
+			if (cn == 0) { output.println(1); }
+			else if (cn < 0) { output.println(0); }
+			else { output.println(0); }
+
+
 		} else {
 			output.println(0);
 		}
